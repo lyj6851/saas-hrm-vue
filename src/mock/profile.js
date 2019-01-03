8685941c481b7e6492e5be136aaee1ce0115c511
Xiaodong Sun <xiaodong.sun@61topbaby.com>
2019-01-03 13:46:00 +0800

    SAAS企业后台管理基本架子搭建

---
+++ src/mock/profile.js
@@ -0,0 +1,13 @@
+export default {
+    profile:{
+        "code" : "10000",
+        "success" : true,
+        "message" : "登录成功",
+        "data" : {
+            mobile:"138000138000",
+            username:"admin",
+            company:"top12345678",
+            email:"bipeng@itcast.cn",
+        }
+    }
+}
